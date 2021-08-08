3d19517a5ee5efdb9e2770c5a2531b865c432c89
Muhammad Farras Ma'ruf <maruffarras@gmail.com>
2021-08-09 04:05:08 +0700

    asd

--- Belajar R/Modul 3/K.B 2/RScript/5. read.table-sep.R
+++
@@ -1,9 +0,0 @@
-#-------------------------------
-# to clear variabel to void misconception
-rm (list=ls())
-
-# seprator adalah karakter "|"
-path <- "Data/nilai_mahasiswa_separator.dat"
-dataMahasiswaSep <- read.table (path, header = TRUE,sep="|")
-dataMahasiswaSep
-#-------------------------------
